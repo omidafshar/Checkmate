d7e847e9ee3e7080f54da7fa2286af9f8ac21af9
MAdhikari <manoj.adhikari@berkeley.edu>
2017-12-05 23:38:06 -0800

    Universal update

--- checkmate/View Controller/userProfileViewController.swift
+++ checkmate/View Controller/userProfileViewController.swift
@@ -21,6 +21,11 @@ class userProfileViewController: UIViewController {
     
     @IBOutlet weak var lastCheckUpdatedLabel: UILabel!
     
+    @IBOutlet weak var DateBase1Button: UIButton!
+    
+    @IBOutlet weak var DateBase2Button: UIButton!
+    
+    @IBOutlet weak var DateBase3Button: UIButton!
     
     
     @IBAction func updateCheckButtonPressed(_ sender: Any) {
@@ -42,7 +47,14 @@ class userProfileViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         userProfileImageView.image = State.shared.currentUser.profilePic
-
+        userNameLabel.text = State.shared.currentUser.First
+        userNameLabel.text = State.shared.currentUser.Last
+        userAgeLabel.text = String(State.shared.currentUser.age)
+        lastCheckUpdatedLabel.text = "Last Checked " + State.shared.currentUser.lastCheckedDate
+        updateCheckButton()
+        updateDetailButtons()
+        
+        
         // Do any additional setup after loading the view.
     }
 
@@ -50,7 +62,65 @@ class userProfileViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
     
+    func  updateCheckButton() {
+        let a = State.shared.currentUser.res1
+        let b = State.shared.currentUser.res2
+        let c = State.shared.currentUser.res3
+        if (a == b && b == c && c == 2) {
+            verificationStatusBar.image = UIImage(named: "noChecks")
+        } else if (a == b && b == c && c == 0) {
+            verificationStatusBar.image = UIImage(named: "verified")
+        } else if (a == 1 || b == 1 || c == 1) {
+            verificationStatusBar.image = UIImage(named: "please_review")
+        }
+    }
+    
+    func updateDetailButtons() {
+        let a = State.shared.currentUser.res1
+        let b = State.shared.currentUser.res2
+        let c = State.shared.currentUser.res3
+        switch a {
+        case 0:
+            DateBase1Button.setImage(UIImage(named: "see_details"), for: [])
+        break
+        case 1:
+            DateBase2Button.setImage(UIImage(named:"see_details_gold"), for: [])
+        break
+        case 2:
+            DateBase3Button.setImage(UIImage(named: "nodetails"), for: [])
+        break
+        default:
+            print("There was an error")
+        }
+        switch b {
+        case 0:
+            DateBase1Button.setImage(UIImage(named: "see_details"), for: [])
+            break
+        case 1:
+            DateBase2Button.setImage(UIImage(named:"see_details_gold"), for: [])
+            break
+        case 2:
+            DateBase3Button.setImage(UIImage(named: "nodetails"), for: [])
+            break
+        default:
+            print("There was an error")
+        }
+        switch c {
+        case 0:
+            DateBase1Button.setImage(UIImage(named: "see_details"), for: [])
+            break
+        case 1:
+            DateBase2Button.setImage(UIImage(named:"see_details_gold"), for: [])
+            break
+        case 2:
+            DateBase3Button.setImage(UIImage(named: "nodetails"), for: [])
+            break
+        default:
+            print("There was an error")
+        }
+    }
 
     /*
     // MARK: - Navigation
